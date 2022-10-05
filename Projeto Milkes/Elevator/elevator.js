10c365e6543006d6e146c3cd0ab8466daea2f7d7
Gabriel Venancio <62064850+venancio0@users.noreply.github.com>
2022-10-05 15:57:08 -0300

    Melhorias - elevador

--- Projeto Milkes/Elevator/elevator.js
+++ Projeto Milkes/Elevator/elevator.js
@@ -21,8 +21,7 @@
     $("#terceiro").click(function(){
       $(".elevador").animate({top: '10px'});
     });
-  }); */
-
+  }); 
   $(document).ready(function(){
     $("#terceiro").click(function(){
       var div = $(".elevador");
@@ -74,3 +73,28 @@
 
     $("#portaDireita").addClass("active-right");   
     $("#portaEsquerda").addClass("active-left"); 
+
+    */
+    var current = 0;
+    $(document).ready(function(){
+    
+      $("#floorSelect li").click(function(){
+      
+        var floor = parseInt($(this).data("floor")),
+         height = floor*20,
+         animate = Math.abs(current-floor)*1000;
+       if(floor == current) return;
+    $("#rightDoor").removeClass("active-right");   
+        $("#leftDoor").removeClass("active-left");  
+        setTimeout(function(){$("#elevatorContainer").css("transition","all "+animate+"ms linear");
+        $("#elevatorContainer").css("bottom",height+"%");
+        current = floor;
+        setTimeout(function(){
+        
+        $("#rightDoor").addClass("active-right");   
+        $("#leftDoor").addClass("active-left");  
+        
+        },animate);},300);
+      });
+    
+    });
